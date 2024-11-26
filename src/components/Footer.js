1790fc057b35b28621ffe46a9a423e7221051c35
Pratik Raj <110324383+pratikraj315@users.noreply.github.com>
2024-11-26 19:49:57 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -52,7 +52,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href=""
+                href="https://www.instagram.com/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
